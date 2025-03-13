4d44b76afd56bc53f0949efe1e98f2650334286d
Forjeon <forjeon22i7@gmail.com>
2025-03-13 15:05:13 -0600

    Finished player walking, sprinting, jumping, and air strafing; next is spherecast to dynamically check for groundedness (currently always assumes player is grounded)

--- jaenks/scripts/player/player.gd
+++ jaenks/scripts/player/player.gd
@@ -1,11 +1,18 @@
 extends RigidBody3D
 
 
-@export var MOVE_SPEED: float = 10.0;
-@export var LOOK_SENSITIVITY: float = 0.005;
+const AIRBORNE_MOVE_FORCE_MULT = 0.1;
+const LOOK_SENSITIVITY = 0.005;
+const GROUNDED_MOVE_FORCE = 1500.0;
+const JUMP_VEL = 5.0;
+const SPRINT_SPEED_MAX_MULT = 2.0;
+const WALK_SPEED_MAX = 5.0;
 
 @onready var player_view = self.find_child("Camera3D");
 
+var do_direct_movement = true;
+var is_grounded = true;
+var is_sprinting = false;
 var view_rotation = Vector2();
 
 
@@ -15,10 +22,19 @@ func _input(event) -> void:
 	if event is InputEventMouseMotion:
 		view_rotation -= event.screen_relative * LOOK_SENSITIVITY;
 		view_rotation.y = clampf(view_rotation.y, -PI/2, PI/2);
+	# Jump input
+	elif event.is_action_pressed("jump"):
+		self.try_jump();
+	# Sprint input
+	elif event.is_action_pressed("sprint"):
+		self.is_sprinting = true;
+	elif event.is_action_released("sprint"):
+		self.is_sprinting = false;
 
 
 func _physics_process(delta) -> void:
-	process_input_move(delta);
+	if self.do_direct_movement:
+		process_input_move(delta);
 	process_look(delta);
 
 
@@ -28,16 +44,28 @@ func _ready() -> void:
 
 # Custom functions
 func process_input_move(delta) -> void:
-	if Input.is_action_pressed("Move Forward"):
-		self.position -= self.transform.basis.z * MOVE_SPEED * delta;
-	if Input.is_action_pressed("Move Backward"):
-		self.position += self.transform.basis.z * MOVE_SPEED * delta;
-	if Input.is_action_pressed("Move Left"):
-		self.position -= self.transform.basis.x * MOVE_SPEED * delta;
-	if Input.is_action_pressed("Move Right"):
-		self.position += self.transform.basis.x * MOVE_SPEED * delta;
+	# Get move direction
+	var input_dir = Input.get_vector("move_left", "move_right", "move_forward", "move_backward");
+	var move_dir = input_dir.x * self.transform.basis.x + input_dir.y * self.transform.basis.z;
+	
+	# Calculate move force based on player state (sprint vs. walk, grounded vs. airborne)
+	var target_move_vel = move_dir * self.WALK_SPEED_MAX;
+	if self.is_sprinting:
+		target_move_vel *= self.SPRINT_SPEED_MAX_MULT;
+	
+	var move_force = (target_move_vel - self.linear_velocity) * self.GROUNDED_MOVE_FORCE;
+	if not self.is_grounded:
+		move_force *= self.AIRBORNE_MOVE_FORCE_MULT;
+	move_force -= move_force.project(self.transform.basis.y);	# Don't damp player vertical velocity
+	
+	self.apply_central_force(move_force * delta);
 
 
 func process_look(delta) -> void:
 	rotation.y = view_rotation.x;
 	player_view.rotation.x = view_rotation.y;
+
+
+func try_jump() -> void:
+	if self.is_grounded:
+		self.apply_central_impulse(Vector3.UP * self.JUMP_VEL);
